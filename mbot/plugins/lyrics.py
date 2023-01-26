fec9049054b3568a345b10a4e314b8d1675c7325
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-01-26 20:25:36 +0530

    Update lyrics.py

--- mbot/plugins/lyrics.py
+++ mbot/plugins/lyrics.py
@@ -14,7 +14,7 @@ async def sng(bot, message):
           try:
               song = message.text.split(None, 1)[1].lower().strip().replace(" ", "%20")
           except IndexError:
-              await message.reply("give me a query eg `lyrics faded`")
+              await message.reply("give me a query eg `lyrics changes`")
           chat_id = message.from_user.id
           rpl = lyrics(song)
           await mee.delete()
