0731ded256a79e412fce9c93ff2bb4aa7d5f8e26
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-01-31 20:20:54 +0530

    Fix✅

--- mbot/plugins/saavan.py
+++ mbot/plugins/saavan.py
@@ -1,6 +1,5 @@
-from pyrogram import Client, filters
-import requests 
-import wget
+from pyrogram import Client,filters
+import wget , requests,os
 @Client.on_message(filters.command('saavn') & filters.text)
 async def song(client, message):
     try:
@@ -12,21 +11,21 @@ async def song(client, message):
         return ""
     pak = await message.reply('Downloading...')
     try:
-        r = requests.get(f"https://saavn.me/search/songs?query={args}")
+        r = requests.get(f"https://saavn.me/search/songs?query={args}&page=1&limit=1").json()
     except Exception as e:
         await pak.edit(str(e))
         return
-    sname = r.json()[1]["name"]
-    slink = r.json()[4]["downloadUrl"]
-    ssingers = r.json()[0]["primaryArtists"]
-    album_id = r.json()[0]["id"]
-    img = r.json()[2]["image"]
+    sname = r['data']['results'][0]['name']
+    slink = r['data']['results'][0]['downloadUrl'][4]['link']
+    ssingers = r['data']['results'][0]['primaryArtists']
+  #  album_id = r.json()[0]["albumid"]
+    img = r['data']['results'][0]['image'][2]['link']
     thumbnail = wget.download(img)
     file = wget.download(slink)
-    ffile = file.replace("mp4", "m4a")
+    ffile = file.replace("mp4", "mp3")
     os.rename(file, ffile)
     await pak.edit('Uploading...')
-    await message.reply_audio(audio=ffile, title=sname, performer=ssingers,caption=f"{sname} - from saavn",thumb=thumbnail)
+    await message.reply_audio(audio=ffile, title=sname, performer=ssingers,caption=f"[{sname}]({k['data']['results'][0]['url']}) - from saavn ",thumb=thumbnail)
     os.remove(ffile)
     os.remove(thumbnail)
     await pak.delete()
