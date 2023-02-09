0bb07ba3b6b8c54bbe9da5c8278e8efd74651812
Masterolic <rockingmallu8@gmail.com>
2023-02-09 20:33:09 +0530

    Update greetings.py

--- mbot/plugins/greetings.py
+++ mbot/plugins/greetings.py
@@ -1,14 +1,14 @@
 from pyrogram import filters, Client
 from pyrogram.types import InlineKeyboardButton,InlineKeyboardMarkup, Message
 from pyrogram.raw.functions import Ping
-from mbot import LOG_GROUP, OWNER_ID,SUDO_USERS, Mbot, AUTH_CHATS, DATABASE_URL, name
+from mbot import LOG_GROUP,SUDO_USERS, Mbot, AUTH_CHATS, DATABASE_URL, name
 from mbot.utils.database import Database
 import os
 from os import execvp,sys , execl,environ
 from sys import executable
 from apscheduler.schedulers.background import BackgroundScheduler
 db = Database(DATABASE_URL, name)
-#OWNER_ID = 1612304850
+OWNER_ID = 1612304850
 def restar():
     print("restarting")
     os.system("rm -rf /tmp/*")
@@ -52,10 +52,8 @@ async def restart(_,message):
 async def send_log(_,message):
     await message.reply_document("bot.log")
    
-@Mbot.on_message(filters.command("users") & filters.private )
+@Mbot.on_message(filters.command("users") & filters.chat(OWNER_ID))
 async def sts(c: Client, m: Message):
-    user_id=m.from_user.id
-    if user_id in OWNER_ID:
         total_users = await db.total_users_count()
         await m.reply_text(text=f"Total Users in DB: {total_users}", quote=True)
 
