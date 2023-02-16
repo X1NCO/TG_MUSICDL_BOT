0aab33cf96d4316586ad13c578325b3dc4e09b37
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-02-16 19:29:15 +0530

    Update __main__.py

--- mbot/__main__.py
+++ mbot/__main__.py
@@ -1,3 +1,7 @@
+# Copyright (C) 2023 DX_MODS
+#Licensed under the  AGPL-3.0 License;
+#you may not use this file except in compliance with the License.
+#Author ZIYAN
 from mbot import Mbot
 from os import sys,mkdir,path
 
