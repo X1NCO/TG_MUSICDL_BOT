fd2a26f365431b1b00a72daf86bae8bec94944eb
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-02-16 19:25:01 +0530

    Update txt.py

--- mbot/utils/txt.py
+++ mbot/utils/txt.py
@@ -1,3 +1,7 @@
+# Copyright (C) 2023 DX_MODS
+#Licensed under the  AGPL-3.0 License;
+#you may not use this file except in compliance with the License.
+#Author ZIYAN
 class dx(object):
     ABOUT_TXT = """
 ╭───────────⍟
