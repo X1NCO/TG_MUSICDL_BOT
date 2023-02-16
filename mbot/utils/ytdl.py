4b616b2de29862abbe40357670482aadded36f13
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-02-16 19:27:47 +0530

    Update ytdl.py

--- mbot/utils/ytdl.py
+++ mbot/utils/ytdl.py
@@ -1,3 +1,7 @@
+# Copyright (C) 2023 DX_MODS
+#Licensed under the  AGPL-3.0 License;
+#you may not use this file except in compliance with the License.
+#Author ZIYAN
 from asgiref.sync import sync_to_async
 from yt_dlp import YoutubeDL
 from requests import get
