90945c82bc966ebc6be5af9e694fd79b5fbcb6df
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-03-08 14:17:52 +0530

    Delete mbot directory

--- mbot/utils/start_time.py
+++
@@ -1,7 +0,0 @@
-# Copyright (C) 2023 DX_MODS
-#Licensed under the  AGPL-3.0 License;
-#you may not use this file except in compliance with the License.
-#Author ZIYAN
-import time
-StartTime = time.time()
-__version__ = 1.1
