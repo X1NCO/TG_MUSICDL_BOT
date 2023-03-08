ad2319b52db59d3b1480e8d7b0fb1e6f40fef640
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-03-08 15:04:36 +0530

    Updated

---
+++ dxbotz/utils/txt.py
@@ -0,0 +1,33 @@
+# Copyright (C) 2023 DX_MODS
+#Licensed under the  AGPL-3.0 License;
+#you may not use this file except in compliance with the License.
+#Author ZIYAN
+class dx(object):
+    ABOUT_TXT = """
+╭───────────⍟
+├🤖 𝙼𝚈 𝙽𝙰𝙼𝙴 : 𝚂𝙿𝙾𝚃𝙸𝙵𝚈 𝙳𝙻
+├👑 𝙳𝙴𝚅𝙴𝙻𝙾𝙿𝙴𝚁𝚂 : <a href=https://t.me/dxziyan>𝚉𝙸𝚈𝙰𝙽</a> 
+├👨‍💻 𝙿𝚁𝙾𝙶𝚁𝙰𝙼𝙴𝚁 : <a href=https://github.com/DX-MODS>𝙳𝚇-𝙼𝙾𝙳𝚂</a>
+├📕 𝙻𝙸𝙱𝚁𝙰𝚁𝚈 : <a href=https://github.com/pyrogram>𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼</a>
+├✏️ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴 : <a href=https://www.python.org>𝙿𝚈𝚃𝙷𝙾𝙽 3</a>
+├💾 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴 : <a href=https://cloud.mongodb.com>𝙼𝙾𝙽𝙶𝙾𝙳𝙱</a>
+├📊 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚄𝚂 : v1.0             
+╰───────────────⍟
+                                """
+    HELP_TXT = """
+🌌 <b><u>HOW TO USE OUR BOT</u></b>
+  
+•> /start to start our bot follow the instructions after.
+•> /to download songs you can send Spotify url here playlist or song url supported flac .
+•> /lyrics and song name to download lyrics of any song.
+•> /ping to check ping of the bot.
+•> /
+ℹ️ 𝗔𝗻𝘆 𝗢𝘁𝗵𝗲𝗿 𝗛𝗲𝗹𝗽 𝗖𝗼𝗻𝘁𝗮𝗰𝘁 :- <a href=https://t.me/DXMODS_Support>𝚂𝚄𝙿𝙿𝙾𝚁𝚃 𝙶𝚁𝙾𝚄𝙿</a>
+"""
+
+#⚠️ don't remove our credits 🙏😢😢
+    DEV_TXT = """
+<b><u>𝚂𝚙𝚎𝚌𝚒𝚊𝚕 𝚝𝚑𝚊𝚗𝚔𝚜 𝚝𝚘 𝚊𝚕𝚕 𝚍𝚎𝚟𝚎𝚕𝚘𝚙𝚎𝚛𝚜</b></u> 
+» 𝗦𝗢𝗨𝗥𝗖𝗘 𝗖𝗢𝗗𝗘 : <a href=https://github.com/DX-MODS/SpotifyDL>𝚂𝙿𝙾𝚃𝙸𝙵𝚈 𝙳𝙻</a>
+» 𝗛𝗢𝗪 𝗧𝗢 𝗗𝗘𝗣𝗟𝗢𝗬 : <a href=https://youtube.com/@DX-MODS</a>
+"""
---
+++ dxbotz/utils/util.py
@@ -0,0 +1,42 @@
+# Copyright (C) 2023 DX_MODS
+#Licensed under the  AGPL-3.0 License;
+#you may not use this file except in compliance with the License.
+#Author ZIYAN
+import shlex
+import math
+import asyncio
+import time
+import aiofiles
+import aiohttp
+#import wget
+import os
+import datetime
+from json import JSONDecodeError
+import requests
+#import ffmpeg
+from pyrogram.errors import FloodWait, MessageNotModified
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
+from youtubesearchpython import VideosSearch
+import yt_dlp
+from youtube_search import YoutubeSearch
+import requests
+from typing import Tuple
+from pyrogram import filters
+from pyrogram import Client
+from dxbotz.utils.shazam import humanbytes, edit_or_reply, fetch_audio
+
+help_message = []
+
+async def run_cmd(cmd: str) -> Tuple[str, str, int, int]:
+    """Run Commands"""
+    args = shlex.split(cmd)
+    process = await asyncio.create_subprocess_exec(
+        *args, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    return (
+        stdout.decode("utf-8", "replace").strip(),
+        stderr.decode("utf-8", "replace").strip(),
+        process.returncode,
+        process.pid,
+    )
