cef2cc91524732442b26fba0af3a72c6a96d1729
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-03-26 16:34:38 +0530

    Update lyrics.py

--- dxbotz/plugins/lyrics.py
+++ dxbotz/plugins/lyrics.py
@@ -27,7 +27,7 @@ async def sng(bot, message):
           except Exception as e:                            
              await message.reply_text(f"lyrics does not found for `{song}` ", quote = True, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs", url = f"https://t.me/Spotify newss")]]))
           finally:
-            await message.reply("Check out @spotify_downloa_bot(music)  @spotifynewss(News)")
+            await message.reply("Check out @dxmodsupdatest(updates)  @dxmodssupoort(support)")
 
 
 
