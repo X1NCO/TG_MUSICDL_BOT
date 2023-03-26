804d719e4bc67e91713e4689605576d2716ec61e
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-03-26 16:32:11 +0530

    Update devtools.py

--- dxbotz/plugins/devtools.py
+++ dxbotz/plugins/devtools.py
@@ -1,3 +1,8 @@
+# Copyright (C) 2023 DX_MODS
+#Licensed under the  AGPL-3.0 License;
+#you may not use this file except in compliance with the License.
+#Author ZIYAN
+
 import sys
 import requests
 import io
