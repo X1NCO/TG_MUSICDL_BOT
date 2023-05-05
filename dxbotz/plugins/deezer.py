a9d53404558d3f3c86e50f7a2e934f16f34793b7
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-05-05 22:03:03 +0530

    Update deezer.py

--- dxbotz/plugins/deezer.py
+++ dxbotz/plugins/deezer.py
@@ -3,11 +3,12 @@
 #you may not use this file except in compliance with the License.
 #Author ZIYAN
 from pyrogram import filters
-from dxbotz import AUTH_CHATS, LOG_GROUP,Dxbotz
+from config import AUTH_CHATS, LOG_GROUP
 from deezer import Client
+from dxbotz import Dxbotz
 from os import mkdir
 from random import randint
-from dxbotz.utils.mainhelper import fetch_tracks,download_songs,parse_deezer_url,thumb_down
+from dxbotz.utils.mainhelper import fetch_tracks,download_dez,parse_deezer_url,thumb_down
 
 
 client = Client()
@@ -27,7 +28,7 @@ async def link_handler(_, message):
             mkdir(randomdir)
             for song in songs:
                 PForCopy = await message.reply_photo(song.get('cover'),caption=f"🎧 Title : `{song['name']}`\n🎤 Artist : `{song['artist']}`\n💽 Album : `{song['album']}`\n💽 Song Number : `{song['playlist_num']}`")
-                path = await download_songs(song,randomdir)
+                path = await download_dez(song,randomdir)
                 thumbnail = await thumb_down(song.get('thumb'),song.get('name'))
                 AForCopy = await message.reply_audio(path,performer=song.get('artist'),title=f"{song.get('name')} - {song.get('artist')}",caption=f"[{song['name']}](https://www.deezer.com/track/{song['deezer_id']}) | {song['album']} - {song['artist']}",thumb=thumbnail,duration=song['duration'])
                 if LOG_GROUP:
