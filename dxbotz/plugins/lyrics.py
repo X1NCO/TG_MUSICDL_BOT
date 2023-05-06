7c07ec6f957b726a6d2198fa2cbacc528790fcee
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-05-06 16:41:21 +0530

    Update lyrics.py

--- dxbotz/plugins/lyrics.py
+++ dxbotz/plugins/lyrics.py
@@ -4,31 +4,29 @@
 #Author ZIYAN
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton 
-from dxbotz import Dxbotz
+
 import requests 
+
 import os
 
 
 API = "https://apis.xditya.me/lyrics?song="
 
-@Dxbotz.on_message(filters.text & filters.command(["lyrics"]) & filters.private)
-async def sng(bot, message):          
-          mee = await message.reply_text("`Searching`")
-          try:
-              song = message.text.split(None, 1)[1].lower().strip().replace(" ", "%20")
-          except IndexError:
-              await message.reply("give me a query eg `lyrics changes`")
+@Client.on_message(filters.text & filters.command(["lyrics"]))
+async def sng(bot, message):
+        if not message.reply_to_message:
+          await message.reply_text("Please reply to a message")
+        else:          
+          mee = await message.reply_text("`Searching 🔎`")
+          song = message.reply_to_message.text
           chat_id = message.from_user.id
           rpl = lyrics(song)
           await mee.delete()
           try:
             await mee.delete()
-            await message.reply(rpl)
+            await bot.send_message(chat_id, text = rpl, reply_to_message_id = message.id, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs ", url = f"t.me/DX_MODS1")]]))
           except Exception as e:                            
-             await message.reply_text(f"lyrics does not found for `{song}` ", quote = True, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs", url = f"https://t.me/Spotify newss")]]))
-          finally:
-            await message.reply("Check out @dxmodsupdatest(updates)  @dxmodssupoort(support)")
-
+             await message.reply_text(f"I Can't Find A Song With `{song}`", quote = True, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs", url = f"t.me/DX_MODS1")]]))
 
 
 def search(song):
@@ -38,5 +36,7 @@ def search(song):
        
 def lyrics(song):
         fin = search(song)
-        text = fin["lyrics"]
+        text = f'**🎶 Sᴜᴄᴄᴇꜱꜰᴜʟʟy Exᴛʀᴀᴄᴛᴇᴅ Lyɪʀɪᴄꜱ Oꜰ {song}**\n\n'
+        text += f'`{fin["lyrics"]}`'
+        text += '\n\n\n**Made By Bixby AI**'
         return text
