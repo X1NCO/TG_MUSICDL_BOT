7521f8f8034a8d0f018d24cf0c865cfc6814d236
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-05-07 13:46:55 +0530

    Fixed

---
+++ dxbotz/plugins/genius.py
@@ -0,0 +1,42 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton 
+from dxbotz import Dxbotz
+from config import genius_api
+import requests 
+from lyricsgenius import Genius 
+import os
+
+API = "https://apis.xditya.me/lyrics?song="
+
+@Dxbotz.on_message(filters.text & filters.command(["genius"]) & filters.private)
+async def sng(bot, message):  
+          genius = Genius(genius_api)        
+          mee = await message.reply_text("`Searching`")
+          try:
+              song = message.text.split(None, 1)[1] #.lower().strip().replace(" ", "%20")
+          except IndexError:
+              await message.reply("give me a query eg `lyrics faded`")
+          chat_id = message.from_user.id
+    #      rpl = lyrics(song)
+          songGenius = genius.search_song(song)
+          rpl = songGenius.lyrics
+          await mee.delete()
+          try:
+            await mee.delete()
+            await message.reply(rpl)
+          except Exception as e:                            
+             await message.reply_text(f"lyrics does not found for `{song} {e}`") #", quote = True, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs", url = f"https://t.me/dxmodsupdates")]]))
+          finally:
+            await message.reply("Check out @dxmodsupdates(updates)")
+
+
+
+def search(song):
+        r = requests.get(API + song)
+        find = r.json()
+        return find
+       
+def lyrics(song):
+        fin = search(song)
+        text = fin["lyrics"]
+        return text
